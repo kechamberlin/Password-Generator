d18315ab3ee676a973c862a3b32c7a25938ea844
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2019-09-24 18:29:25 -0700

    Added variables to the javascript file.

---
+++ script.js
@@ -0,0 +1,9 @@
+// ========== Variables ==========
+    // Create variables by targeting their IDs from the HTML document.
+var form = document.getElementById("passwordGenerationForm");
+var passwordTextArea = document.getElementById("password");
+var passwordLengthCheckbox = document.getElementById("length");
+
+
+// ========== Functions ==========
+
