4a02b291ee88ba26d8bf60da1dfe2d91f41c450a
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2019-09-24 22:23:24 -0700

    Moved script.js file to a script tag in the html because for some reason it was not working properly.

--- script.js
+++
@@ -1,9 +0,0 @@
-// ========== Variables ==========
-    // Create variables by targeting their IDs from the HTML document.
-var form = document.getElementById("passwordGenerationForm");
-var passwordTextArea = document.getElementById("password");
-var passwordLengthCheckbox = document.getElementById("length");
-
-
-// ========== Functions ==========
-
